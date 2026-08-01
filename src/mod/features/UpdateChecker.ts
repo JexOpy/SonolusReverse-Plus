0e7e6015e7577e52c406afdd8918d7751949be04
repinek <137826826+repinek@users.noreply.github.com>
2026-08-01 17:50:18 +0500

    feat: remove version check in non release builds

--- src/mod/features/UpdateChecker.ts
+++ src/mod/features/UpdateChecker.ts
@@ -1,6 +1,7 @@
 import { UnityWebRequest } from "../../engine/wrappers/UnityWebRequest";
 import { Logger } from "../../utils/Logger";
 import { Constants } from "../data/Constants";
+import { ModPreferences } from "../data/ModPreferences";
 
 interface VersionResponse {
     major: number;
@@ -11,13 +12,19 @@ export class UpdateChecker {
     private static _latestVersion: string | null = null;
 
     static async checkVersion(): Promise<void> {
+        if (ModPreferences.ENV !== "release") {
+            this._latestVersion = ModPreferences.VERSION;
+            Logger.warn("[UpdateChecker::checkVersion] Skipping version check in non release builds");
+            return;
+        }
+
         try {
             const request = await UnityWebRequest.sendGet(Constants.VERSION_URL);
             const text = request.text;
             const response = JSON.parse(text) as VersionResponse;
             this._latestVersion = `${response.major}.${response.minor}.0`;
             request.dispose();
-            Logger.info(`Latest version fetched: ${this._latestVersion}`);
+            Logger.info(`[UpdateChecker::checkVersion] Latest version fetched: ${this._latestVersion}`);
         } catch (e) {
             Logger.error(`[UpdateChecker::checkVersion] Failed: ${e}`);
         }
