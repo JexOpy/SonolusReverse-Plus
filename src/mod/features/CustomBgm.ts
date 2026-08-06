131ce050e5d24f7330e02f84710c5b4063c3c7b1
repinek <137826826+repinek@users.noreply.github.com>
2026-08-06 23:47:49 +0500

    fix(CustomBgm): rewrite hook with Interceptor attach
    Highly recommend to read the comment

--- src/mod/features/CustomBgm.ts
+++ src/mod/features/CustomBgm.ts
@@ -4,47 +4,69 @@ import { Logger } from "../../utils/Logger";
 import { Config } from "../data/Config";
 
 export class CustomBgm {
-    private static _validatedPath: string = "";
-
     private static PreviewSystem: Il2Cpp.Class | null;
+
+    private static pendingPath: string | null = null;
 
     static init(): void {
         this.PreviewSystem = AssemblyHelper.AssemblyCSharp.class("Sonolus.Preview.PreviewSystem");
 
         const BgmPlayer = AssemblyHelper.AssemblyCSharp.class("Sonolus.Audio.BgmPlayer");
 
-        // @ts-ignore
-        BgmPlayer.method<Il2Cpp.Object>("Create", 8).implementation = this.createHook;
+        // Sonolus.Audio.BgmPlayer Create(string path, long startTime, float bgmTime, float speed, float volume, bool loop, float loopTime, bool isPrecise);
+        const Create = BgmPlayer.method<Il2Cpp.Object>("Create", 8);
+
+        // Why `Interceptor.attach` over `Create.implementation`?
+        //
+        // Implementation FULLY replaces method, so it required to re-invoke
+        // original method using `method.invoke` which calls internal `invokeRaw`
+        //
+        // For some reason when we call it so many times something breaks
+        // and we got `access violation accessing` 0x0 on `invokeRaw`
+        //
+        // In `args` argument we have General Purpose registers (GP)
+        // Floating points arguments passed in SIMD registers and Floating Pointers registers (FP)
+        //
+        // Look at the arguments:
+        // str, long, float, float, float, bool, float, bool
+        // x0 , x1  , s0   , s1   , s2   , x2  , s3   , x3
+        //
+        // From: https://github.com/ARM-software/abi-aa/blob/main/aapcs64/aapcs64.rst#68parameter-passing
+        // Floating-point and short vector types are passed in SIMD and Floating-point registers or on the stack; never in general-purpose registers (except when they form part of a small structure that is neither an HFA nor an HVA).
+        //
+        // This is hack, but at least it's works
+        // Sonolus compiled only for `arm64` and `arm`
+        // so we are safe with that move
+        //
+        // Written by hand, not AI comment
+        // Thanks TSoding code sessions, at least I know about FP registers
+        Interceptor.attach(Create.virtualAddress, {
+            onEnter(this: InvocationContext, args: InvocationArguments) {
+                Logger.debug("BgmPlayer::Create called");
+
+                const path = new Il2Cpp.String(args[0]);
+
+                if (path.isNull() || !CustomBgm.shouldOverride(path.content)) {
+                    return;
+                }
+
+                CustomBgm.pendingPath = Config.customBgmPath;
+                args[0] = Il2Cpp.string(CustomBgm.pendingPath).handle;
+
+                // loopTime
+                switch (Process.arch) {
+                    case "arm64":
+                    case "arm":
+                        // Or Arm64CpuContext
+                        (this.context as ArmCpuContext).s3 = 0;
+                        break;
+                    default:
+                        Logger.error("[CustomBgm::CreateHook] Unsupported architecture. How you actually run Sonolus?");
+                }
+            }
+        });
 
         Logger.info("[CustomBgm::init] Initialized");
-    }
-
-    // TODO: rewrite this uhh stuff
-    private static createHook(
-        this: Il2Cpp.Object,
-        path: Il2Cpp.String,
-        startTime: number,
-        bgmTime: number,
-        speed: number,
-        volume: number,
-        loop: boolean,
-        loopTime: number,
-        isPrecise: boolean
-    ): Il2Cpp.Object {
-        Logger.hook("BgmPlayer::Create called");
-        if (path.isNull()) return this.method<Il2Cpp.Object>("Create", 8).invoke(path, startTime, bgmTime, speed, volume, loop, loopTime, isPrecise);
-
-        const originalPath = path.content;
-
-        if (CustomBgm.shouldOverride(originalPath)) {
-            Logger.debug(`[CustomBgm::createHook] Overriding UI BGM to ${Config.customBgmPath}`);
-            path = Il2Cpp.string(Config.customBgmPath);
-
-            // Reset the loop offset which is normally ~15.36s for the default BGM
-            loopTime = 0.0;
-        }
-
-        return this.method<Il2Cpp.Object>("Create", 8).invoke(path, startTime, bgmTime, speed, volume, loop, loopTime, isPrecise);
     }
 
     private static shouldOverride(originalPath: string | null): boolean {
